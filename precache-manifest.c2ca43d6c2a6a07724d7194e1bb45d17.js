278335d9d3ebb7aaabc3854ee5cdee08189a30f9
sergeysova <5620073+sergeysova@users.noreply.github.com>
2021-08-24 09:54:45 +0000

    Deploying to gh-pages from  @ 5d0d7d35f6a2b5a1860cf720621cf290c4e5cff8 🚀

---
+++ precache-manifest.c2ca43d6c2a6a07724d7194e1bb45d17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72580ea12eb783a82f4c80e05675f46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e8af9afc8df138f7a97",
+    "url": "/static/css/main.bbbbc01d.chunk.css"
+  },
+  {
+    "revision": "e53eee85fa038f3617bb",
+    "url": "/static/js/2.ae9d3578.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.ae9d3578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e8af9afc8df138f7a97",
+    "url": "/static/js/main.27a6270b.chunk.js"
+  },
+  {
+    "revision": "827eeebeab18743796e4",
+    "url": "/static/js/runtime-main.dcb7a3af.js"
+  }
+]);
