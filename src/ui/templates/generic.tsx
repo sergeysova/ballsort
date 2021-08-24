1ef7ad6862dd0ad6a618c92c16756896f03cf99d
Sergey Sova <5620073+sergeysova@users.noreply.github.com>
2021-08-24 13:00:12 +0300

    fix: change link to source code and effector

--- src/ui/templates/generic.tsx
+++ src/ui/templates/generic.tsx
@@ -5,8 +5,8 @@ export const GenericTemplate: React.FC = ({ children }) => (
   <>
     <main>{children}</main>
     <nav className="navigation">
-      <a href="https://effector.now.sh">Effector</a>
-      <a href="https://github.com/sergeysova/ball.sova.dev">Source Code</a>
+      <a href="https://effector.dev">Effector</a>
+      <a href="https://github.com/sergeysova/ballsort">Source Code</a>
     </nav>
   </>
 );
